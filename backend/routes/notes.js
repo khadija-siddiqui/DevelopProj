869b4628142ecbf3071e14eb0f49b48c87a5b9e0
Khadija <khadijasiddiqui183@gmail.com>
2024-05-08 19:48:28 -0400

    added CRUD operations - GET, POST, DELETE

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -8,14 +8,45 @@
 // DELETE -- deletes note by its ID from the database
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const Note = require('../model/Note');
 
 //GET all notes
 router.get('/', async(req, res) => {
-
+    try {
+        const notes = await Note.find();
+        res.json(notes);
+    }catch (err){
+        res.status(500).json({message: err.message});
+    }
 }); 
 
 //POST a new note
+router.post('/', async (req, res) => {
+    const note = new Note({
+        _id: new mongoose.Types.ObjectId(),
+        title: req.body.title,
+        content: req.body.content,
+    });
 
-
+    try {
+        const newNote = await note.save();
+        res.status(201).json(newNote);
+    } catch (err) {
+        res.status(400).json({message: err.message});
+    }
+});
 //DELETE a note
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleteNote = await Note.deleteOne({ _id: req.params.id});
+        if(deleteNote.deletedCount === 0) {
+            return res.status(404).json({message: "Note not found"});
+        }
+        res.json({message: 'Note deleted'});
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+});
+
+module.exports = router;
