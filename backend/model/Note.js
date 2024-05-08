471de9f4cbaa52f0322b63db70bc5008294a06de
Khadija <khadijasiddiqui183@gmail.com>
2024-05-08 14:49:19 -0400

    updated server.js & model files

--- backend/model/Note.js
+++ backend/model/Note.js
@@ -2,3 +2,19 @@
 //specifing ea note obj stored in MongoDB database 
 //  including: title and content
 
+const mongoose = require('mongoose');
+
+//defining schema
+const noteSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId, 
+    title: {
+        type: String,
+    },
+    content: {
+        type: String,
+    },
+});
+
+//creating & exporting model based on schema
+module.exports = mongoose.model('Note', noteSchema);
+
--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,35 @@
 //connecting to MongoDB database
 //defining routes for handling API endpts related to notes
 //starts server to listen to incoming requests
+
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+//uri to connect to mongoDB
+const uri = //link from mongodb - adjust <password> to be password for database user
+
+//function to connect to mongoDB
+async function connect() {
+    try {
+        await mongoose.connect(uri)
+        console.log("Connected to MongoDB");
+    } catch (error){
+        console.error("MongoDB connection error: ", error);
+        process.exit(1); //unable to connect - exit 
+    }
+}
+
+connect();
+
+//defining routes for handling API endpts related to notes
+const noteRouter = require('./routes/notes');
+app.use('/notes', noteRouter);
+
+
+//start server
+app.listen(PORT, () => {
+    console.log("Server started on port ${PORT}`");
+});
+
